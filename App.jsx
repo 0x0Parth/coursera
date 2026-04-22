b6d4093a310f6353e0c44db512d993e96f6823c5
0x0Parth <168005323+0x0Parth@users.noreply.github.com>
2026-04-22 21:06:39 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,27 +1,5 @@
-// Task 4: App.jsx
-import React, { useState } from 'react';
-import ProductList from './ProductList';
-import './App.css';
-
-function App() {
-  const [showProductList, setShowProductList] = useState(false);
-
-  return (
-    <div className="app-container">
-      {!showProductList ? (
-        <div className="landing-page"> {/* This class is required for Task 3/4 */}
-          <div className="content">
-            <h1>Paradise Nursery</h1>
-            <p>Where Greenery Meets Peace</p>
-            <button className="get-started-button" onClick={() => setShowProductList(true)}>
-              Get Started
-            </button>
-          </div>
-        </div>
-      ) : (
-        <ProductList />
-      )}
-    </div>
-  );
-}
-export default App;
+// Task 4 snippet
+<div className="landing-page">
+   <h1>Paradise Nursery</h1>
+   <button onClick={() => setShowProductList(true)}>Get Started</button>
+</div>
